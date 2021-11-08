ffa977539ab93368bcd473c9c6b7122b144edf69
taniyag-hexaware <TaniyaG@hexaware.com>
2021-11-08 15:41:31 +0530

    front

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -7,29 +7,29 @@ import { map } from 'rxjs/operators';
   providedIn: 'root',
 })
 export class ApiService {
-  private baseUrl = environment.apiUrl;
+  // private baseUrl = environment.apiUrl;
 
-  constructor(private _http: HttpClient) {}
+  // constructor(private _http: HttpClient) {}
 
-  getTypeRequest(url: string) {
-    return this._http.get(`${this.baseUrl}${url}`).pipe(
-      map((res) => {
-        return res;
-      })
-    );
-  }
-  postTypeRequest(url: string, payload: any) {
-    return this._http.post(`${this.baseUrl}${url}`, payload).pipe(
-      map((res) => {
-        return res;
-      })
-    );
-  }
-  putTypeRequest(url: string, payload: any) {
-    return this._http.put(`${this.baseUrl}${url}`, payload).pipe(
-      map((res) => {
-        return res;
-      })
-    );
-  }
+  // getTypeRequest(url: string) {
+  //   return this._http.get(`${this.baseUrl}${url}`).pipe(
+  //     map((res) => {
+  //       return res;
+  //     })
+  //   );
+  // }
+  // postTypeRequest(url: string, payload: any) {
+  //   return this._http.post(`${this.baseUrl}${url}`, payload).pipe(
+  //     map((res) => {
+  //       return res;
+  //     })
+  //   );
+  // }
+  // putTypeRequest(url: string, payload: any) {
+  //   return this._http.put(`${this.baseUrl}${url}`, payload).pipe(
+  //     map((res) => {
+  //       return res;
+  //     })
+  //   );
+  // }
 }
